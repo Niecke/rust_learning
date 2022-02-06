b34d6ce22da24a8105856c5bd76cc4a765f21c69
Niecke <daniel@niecke-it.de>
2022-02-06 17:56:07 +0000

    day008

---
+++ day008/example/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+struct Rectangle<T, U> {
+    width: T,
+    height: U,
+}
+
+impl<T, U> Rectangle<T, U> {
+    fn get_width(&self) -> &T {
+        &self.width
+    }
+}
+
+impl Rectangle<u8, u8> {
+    fn get_perimeter(&self) -> u8 {
+        2 * self.width + 2 * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 1u8,
+        height: 3u16,
+    };
+    println!("rect is {:?}", rect);
+
+    println!("width is {}", rect.get_width());
+
+    let rect2 = Rectangle {
+        width: 1u8,
+        height: 3u8,
+    };
+    println!("perimeter is {}", rect2.get_perimeter());
+}
